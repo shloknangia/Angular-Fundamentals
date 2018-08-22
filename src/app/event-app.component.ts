1382f99e8bc578566d2801659e57c411b8341a00
Shlok Nangia <SNangia@slb.com>
2018-08-22 16:31:29 +0530

    First Commit

---
+++ src/app/event-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template:'<events-list></events-list>'
+  /*template: '<h2>Hello World</h2>   <img src="/assets/images/basic-shield.png"/>',*/
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
