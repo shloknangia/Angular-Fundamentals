bc48aecbf70ae1173fc128b028e75bd5d0f05cfc
Shlok Nangia <SNangia@slb.com>
2018-08-23 14:51:36 +0530

    4.7

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -9,11 +9,11 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
                 <div>Date: {{event.date}}</div>
                 <div>Time: {{event.time}}</div>
                 <div>Price: \${{event.price}}</div>
-                <div *ngIf="event?.location">
+                <div [hidden]="!event?.location">
                     <span>Location: {{event.location?.address}}</span>
                     <span class="pad-left">{{event.location?.city}}, {{event.location?.country}}</span>
                 </div>
-                <div *ngIf="event?.onlineUrl">Online Url: {{event?.onlineUrl}}</div>
+                <div [hidden]="!event?.onlineUrl">Online Url: {{event?.onlineUrl}}</div>
             </div>
             <!--<button class="btn btn-primary" (click)="handleClickMe()">Click Me!</button>-->
         </div>
