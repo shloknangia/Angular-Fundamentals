0f295dda18f5ff529f137d8f537db5374e5c8a6e
Shlok Nangia <SNangia@slb.com>
2018-08-25 17:09:12 +0530

    6.6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ToastrService } from './common/toastr.service';
 import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { appRoutes } from './routes'
 import { RouterModule } from '@angular/router';
+import { CreateEventComponent } from './events/create-event.component'
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { RouterModule } from '@angular/router';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    CreateEventComponent
   ],
   imports: [
     BrowserModule,
