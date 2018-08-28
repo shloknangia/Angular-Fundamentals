6e65e6f8d5b41892df46e1db85c262a29dc712b2
Shlok Nangia <SNangia@slb.com>
2018-08-28 14:11:58 +0530

    7.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RouterModule } from '@angular/router';
 import { Error404Component } from './errors/404.component'
 // import { EventRouteActivator } from './events/event-details/event-route-activator.service'
 // import { EventListResolver } from './events/events-list-resolver.service'
+import { AuthService } from './user/auth.service'
 
 import { 
   EventsListComponent,
@@ -45,6 +46,7 @@ import {
     EventService, 
     ToastrService, 
     EventRouteActivator,
+    AuthService,
     EventListResolver,
     { 
       provide: 'canDeactivateCreateEvent', 
--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core'
+import { AuthService } from './auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
-    username
-    password
+    username;
+    password;
+
+    constructor(private authService: AuthService, private router: Router){
+
+    }
     
     login(formValues){
-        console.log(formValues)    ;
+        // console.log(formValues)    ;
+        this.authService.loginUser(formValues.userName, formValues.password);
+        this.router.navigate(['/events']);
+    }
+
+    cancel(){
+        this.router.navigate(['/events']);
     }
 }
