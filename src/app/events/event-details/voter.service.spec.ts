3be8ee020525c0a505a06051ebc53eb6a4b1d048
Shlok Nangia <SNangia@slb.com>
2018-09-16 14:35:49 +0530

    15.4

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -20,6 +20,26 @@ describe('voterService', () => {
 
             expect(session.voters.length).toBe(1);
             expect(session.voters[0]).toBe("john");
+        });
+
+        it('should call http.delete with right URL', () => {
+            var session = { id: 6, voters: ["joe", "john"] };
+            mockHttp.delete.and.returnValue(of(false));
+            voterService.deleteVoter(3, <ISession>session, "joe");
+
+            expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe');
+        })
+
+    })
+
+
+    describe('addVoter', () => {
+        it('should call http.post with right URL', () => {
+            var session = { id: 6, voters: ["john"] };
+            mockHttp.post.and.returnValue(of(false));
+            voterService.addVoter(3, <ISession>session, "joe");
+
+            expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe', {}, jasmine.any(Object));// as we can assume 3 arg will be right as not very complex
         })
     })
 
